ec39dbdbb4314ea4bd5a45d72a085b0cf2a0252e
mcbarlowe <mcbarlowe@gmail.com>
2019-01-04 01:45:32 -0500

    code to create home/away abbrev/id, event, event_team, shot_made, and is_block columns

--- scraper.py
+++ scraper.py
@@ -32,7 +32,7 @@ shot_type_dict = {58: 'turnaround hook shot', 5: 'layup', 6: 'driving layup',
 
 #this dictionary will categorize the event types that happen in the NBA
 #play by play
-event_type_dict = {1: 'shot', 2: 'shot', 4: 'rebound', 5: 'turnover',
+event_type_dict = {1: 'shot', 2: 'missed_shot', 4: 'rebound', 5: 'turnover',
                    20: 'stoppage: out-of-bounds', 6: 'foul', 3: 'free-throw',
                    8: 'substitution', 12: 'period-start', 10: 'jump-ball',
                    9: 'team-timeout', 18: 'instant-replay', 13: 'period-end',
@@ -80,20 +80,93 @@ def scrape_pbp(game_id, user_agent):
     for qtr in range(len(pbp_dict['g']['pd'])):
         pbp_df_list.append(pd.DataFrame(pbp_dict['g']['pd'][qtr]['pla']))
 
+#pulling the home and away team abbreviations and the game date
+    gcode = pbp_dict['g']['gcode'].split('/')
+    date = gcode[0]
+    teams = gcode[1]
+    home_team = teams[3:]
+    away_team = teams[:3]
     pbp_df = pd.concat(pbp_df_list)
 
 #joining the two dataframes together and only pulling in relavent columns
     clean_df = pbp_v2_df.merge(pbp_df[['evt', 'locX', 'locY', 'hs', 'vs', 'de']],
                                left_on = 'EVENTNUM', right_on='evt')
 
+#add date and team abbrev columns to dataframe
+    clean_df.loc[:, 'home_team_abbrev'] = home_team_abbrev
+    clean_df.loc[:, 'away_team_abbrev'] = away_team_abbrev
+    clean_df.loc[:, 'game_date'] = date
+
+#code to properly get the team ids as the scientific notation cuts off some digits
+    home_team_id = clean_df[clean_df.PLAYER1_TEAM_ABBREVIATION == home_team_abbrev].PLAYER1_TEAM_ID.astype(int).unique()
+    away_team_id = clean_df[clean_df.PLAYER1_TEAM_ABBREVIATION == away_team_abbrev].PLAYER1_TEAM_ID.astype(int).unique()
+    clean_df.loc[:, 'home_team_id'] = home_team_id
+    clean_df.loc[:, 'away_team_id'] = away_team_id
+
+#create an event team colum
+    clean_df['event_team'] = np.where(clean_df['HOMEDESCRIPTION'].isnull(),
+                                    clean_df['home_team_abbrev'], clean_df['away_team_abbrev'])
+
+#create and event type description column
+    clean_df['event_type_de'] = clean_df[['etype']].replace({'etype': event_type_dict})
+
+#create an event team colum
+    clean_df['event_team'] = np.where(clean_df['HOMEDESCRIPTION'].isnull(),
+                                      clean_df['home_team_abbrev'],
+                                      clean_df['away_team_abbrev'])
+
+#create column whether shot was succesful or not
+    def made_shot(row):
+    '''
+    function to determine whether shot was made or missed
+
+    Input:
+    row - pandas row
+
+    Output:
+    shot_made - binary variable
+    '''
+
+    if row['event_type_de'] == 'shot':
+        return 1
+    elif row['event_type_de'] == 'missed_shot':
+        return 0
+    elif (row['event_type_de'] == 'free-throw') & ('Missed' in row['de']):
+        return 0
+    elif (row['event_type_de'] == 'free-throw') & ('Missed' not in row['de']):
+        return 1
+    else:
+        return np.nan
+
+    clean_df['shot_made'] = clean_df.apply(made_shot, axis=1)
+
+#create a column that says whether the shot was blocked or not
+    clean_df['is_block'] = np.where(clean_df.HOMEDESCRIPTION.str.contains('BLOCK') |
+                                    clean_df.VISITORDESCRIPTION.str.contains('BLOCK'),
+                                    1, 0)
+
 #TODO Clean time to get a seconds elapsed column
 
-#TODO parse etype column to get summary description of event happening for factoring
 
 #TODO parse mtype column to get all the shot types being taken
 
-#TODO create column whether shot was succesful or not
 
 #TODO create columns to tell which players are on the court for every given event
 #probably need to get starting lineups and player ids of each quarter for this and join on period starts
-    kkkkkkkk
+
+#this is the api endpoint to get the starting lineup for each respective
+#period. It takes a team id in the OpponentTeamID variable, a period number
+#for the period variable this can be 1-4 or 5, 6, 7 if OT was played. A season
+#variable in the form of YYYY-YY so for the 2019 season it would be 2018-19.
+#It aslo will need a start and end date for the lineups. Since this scrape will
+#only want the line ups for the game played on said date we are scraping the
+#DateFrom and DateTo variables will be the same. These will be to be in the form
+#YYYY-MM-DD
+#TODO create the variables from the play by play dataframe needed for the
+    lineup_api = ('https://stats.nba.com/stats/leaguedashlineups?Conference=&'
+                 f'DateFrom={start_date}&DateTo={end_date}&Division=&'
+                 'GameSegment=&GroupQuantity=5&LastNGames=0&LeagueID=&Location=&'
+                 f'MeasureType=Base&Month=0&OpponentTeamID={team_id}&Outcome=&PORound=&'
+                 f'PaceAdjust=N&PerMode=Totals&Period={period}&PlusMinus=N&Rank=N&'
+                 f'Season={season}&SeasonSegment=&SeasonType=Regular+'
+                 'Season&ShotClockRange=&TeamID=&VsConference=&VsDivision=')
