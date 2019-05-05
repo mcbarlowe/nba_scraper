a25fb028a3c37d9e09a76a5692ea9d67f796fd16
mcbarlowe <mcbarlowe@gmail.com>
2019-05-04 21:33:07 -0400

    ugh

--- test.py
+++ test.py
@@ -7,8 +7,6 @@ import nba_scraper.nba_scraper as ns
 def test_test():
     print("this is a test")
 
-'''
 def test_scrape_return():
     dataframe = ns.scrape_game([21800001])
     assert isinstance(dataframe, pd.DataFrame)
-'''
