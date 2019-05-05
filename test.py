1a76bd1756ebc516fc8f132152a003edb4cb4858
mcbarlowe <mcbarlowe@gmail.com>
2019-05-04 20:29:16 -0400

    Added first test and integration with Travis CI

---
+++ test.py
@@ -0,0 +1,9 @@
+'''
+Test functions for my nba_scraper module
+'''
+import pandas as pd
+import nba_scraper.nba_scraper as ns
+
+def test_scrape_return():
+    dataframe = ns.scrape_game([21800001])
+    assert isinstance(dataframe, pd.DataFrame)
