771dd7a43ef310ac67acf224eba333d97d229d22
mcbarlowe <mcbarlowe@gmail.com>
2019-05-04 23:53:00 -0400

    change some travis things and test

--- test.py
+++ test.py
@@ -5,5 +5,7 @@ import pandas as pd
 import nba_scraper.nba_scraper as ns
 
 def test_scrape_return():
+    print("starting test")
     dataframe = ns.scrape_game([21800001])
+    print("testing assertion that dataframe was returned")
     assert isinstance(dataframe, pd.DataFrame)
