42abe0c4a02179e01d49d91fd26dcb1a93406555
mcbarlowe <mcbarlowe@gmail.com>
2020-02-04 20:24:38 -0500

    fixed issue where season isn't formatted properly

--- nba_scraper/scrape_functions.py
+++ nba_scraper/scrape_functions.py
@@ -198,7 +198,9 @@ def scrape_pbp(v2_dict):
     elif clean_df["game_id"].unique()[0][3:5] == "00":
         clean_df["season"] = 2001
     else:
-        clean_df.loc[:, ("season")] = f"20{int(clean_df['game_id'].unique()[0][3:5])+1}"
+        clean_df.loc[
+            :, ("season")
+        ] = f"20{int(clean_df['game_id'].unique()[0][3:5])+1:02}"
     # TODO columns to pull out [['evt', 'locX', 'locY', 'hs', 'vs', 'de']]
     # create an event team colum
     clean_df["event_team"] = np.where(
